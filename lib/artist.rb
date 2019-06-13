ac2cb081148ea7ed98d9ac2f7a62945191c2e600
Annie Truex <artruex@gmail.com>
2019-06-13 23:14:24 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,10 +12,11 @@ class Artist
 
   @@artists = []
 
-  #def initialize
+  def initialize
   #  @@artists << self
-  #  @songs = []
-  #end
+    super
+    @songs = []
+  end
 
   #def self.find_by_name(name)
   #  @@artists.detect{|a| a.name == name}
